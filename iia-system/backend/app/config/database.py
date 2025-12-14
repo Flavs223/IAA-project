091683a9748408faa6168b59681475075bb774ad
Flavs223 <barbozaflavio25@gmail.com>
2025-12-13 21:05:12 -0600

    Add config module structure

---
+++ iia-system/backend/app/config/database.py
@@ -0,0 +1,3 @@
+""" Este archivo me permitirá leer las variables de entorno, crear la conexión a la base de datos y devolver la sesión de la base de datos,
+exponer una función reutilizable para interactuar con la base de datos en otras partes de la aplicación, manejar errores,
+permitir transacciones y asegurar que las conexiones se cierren adecuadamente. """
